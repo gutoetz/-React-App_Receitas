813f1f986df67aa322f9a21f57502c63f8020ce7
André Pestana Silva <atlas.ifpa@gmail.com>
2022-11-01 17:23:29 -0300

    Testes de DoneRecipes feito, faltando apenas testes para BodyRecipes

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -3,30 +3,6 @@ import DoneRecipesCard from '../components/DoneRecipesCard';
 import Header from '../components/Header';
 
 function DoneRecipes() {
-  // const mock = [
-  //   {
-  //     id: '52771',
-  //     type: 'meal',
-  //     nationality: 'Italian',
-  //     category: 'Vegetarian',
-  //     alcoholicOrNot: '',
-  //     name: 'Spicy Arrabiata Penne',
-  //     image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-  //     doneDate: '23/06/2020',
-  //     tags: ['Pasta', 'Curry'],
-  //   },
-  //   {
-  //     id: '178319',
-  //     type: 'drink',
-  //     nationality: '',
-  //     category: 'Cocktail',
-  //     alcoholicOrNot:  'Alcoholic',
-  //     name: 'Aquamarine',
-  //     image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-  //     doneDate: '23/06/2020',
-  //     tags: [],
-  //   },
-  // ];
   // States
   const [doneRecipes, setDoneRecipes] = useState();
   const [defaultDoneRecipes, setDefaultDoneRecipes] = useState();
@@ -75,7 +51,7 @@ function DoneRecipes() {
           Drinks
         </button>
       </div>
-      <div>
+      <div data-testid="done-recipes-container">
         {doneRecipes && doneRecipes.map((recipes, index) => (
           <DoneRecipesCard key={ index } recipes={ recipes } index={ index } />
         ))}
